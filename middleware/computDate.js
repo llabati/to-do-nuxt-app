198e746f48c392c8cbba15be34d1814bff374fec
llabati <33926231+llabati@users.noreply.github.com>
2019-08-04 16:07:28 +0200

    How many days ? Simplify this comput !
    Changes avoid repetitions in code. Simple, better...

--- middleware/computDate.js
+++ middleware/computDate.js
@@ -1,31 +1,20 @@
 function compareToNow(todo){
 
     let dayNow = new Date().getDate()
-    let restDays = (Number(todo.day) - dayNow) === 0 ? 0 : (Number(todo.day) - dayNow) - 1
-    console.log(restDays)
-    let month31 = [ '01', '03', '05', '07', '08', '10', '12' ]
-    let month30 = [ '04', '06', '09', '11' ]
-    let month28 = [ '02' ]
-    let delay = 0
-
-    if (restDays > 0) return `Il vous reste ${restDays} jours pour terminer cette tâche.`
-    if (restDays === 0) return `Cette tâche doit être terminée aujourd'hui.`
-    if (restDays < 0) {
-        if ( month31.includes(todo.month) ) { 
-            delay = (31 - dayNow) + Number(todo.day - 1)
-            return `Vous avez encore ${delay} jours pour terminer cette tâche.`
-        }
-        if ( month30.includes(todo.month) ) { 
-            delay = (30 - dayNow) + Number(todo.day - 1)
-            return `Vous avez encore ${delay} jours pour terminer cette tâche.`
-        }
-        if ( month28.includes(todo.month) ) { 
-            delay = (28 - dayNow) + Number(todo.day - 1)
-            return `Vous avez encore ${delay} jours pour terminer cette tâche.`
-        }
+    let monthNow = new Date().getMonth() + 1
+    let yearNow = new Date().getFullYear()
+    let nMonths = todo.month - monthNow
+    let restDays
+    if (todo.year === yearNow){
+        restDays = ((nMonths * 30) + (todo.day - dayNow) === 0) ? 0 : (nMonths * 30) + (todo.day - dayNow) - 1     
+        if (restDays === 0) return `Cette tâche doit être terminée aujourd'hui`       
+    } else {
+        restDays = 365 + (nMonths * 30) + (todo.day - dayNow) - 1  
     }
-
+    return `Il vous reste ${restDays} jours pour terminer cette tâche.`
 }
+
+
 
 export { compareToNow }
 
